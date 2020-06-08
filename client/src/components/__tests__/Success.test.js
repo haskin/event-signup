8b89f85ccd231e1e2a874d9aea2620816ad2ac65
haskin <adrian4k9@gmail.com>
2020-06-08 16:05:51 -0400

    Added snapshot test for Registrees.

--- client/src/components/__tests__/Success.test.js
+++ client/src/components/__tests__/Success.test.js
@@ -6,6 +6,5 @@ describe('Snapshot for Success component', () => {
   it('renders correctly', () => {
     const wrapper = shallow(<Success />);
     expect(wrapper).toMatchSnapshot();
-    // On the first run of this test, Jest will generate a snapshot file automatically.
   });
 });
