1d620349d28d6d203538a57981f78eb83e4a5734
haskin <adrian4k9@gmail.com>
2020-06-08 20:58:53 -0400

    Added code for heroku to create build of static files and server them.

--- server.js
+++ server.js
@@ -66,6 +66,17 @@ app.post('/api/registrees', async (req, res) => {
 	};
 });
 
+// Server static assets
+const path = require('path');
+if(process.env.NODE_ENV === 'production'){
+	// Static folder
+	app.use(express.static('client/build'));
+	// Any path that isn't API
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`)
